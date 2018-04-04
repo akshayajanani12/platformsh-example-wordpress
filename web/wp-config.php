efa15d733cb99c0a255a8a451cc29d5c64de372a
Christopher Skene <chris@xtfer.com>
2018-04-04 13:23:49 +1000

    fix wp-settings.php

--- web/wp-config.php
+++ web/wp-config.php
@@ -99,5 +99,5 @@ if ( !defined('ABSPATH') )
     define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once(ABSPATH . 'wp/wp-settings.php');
 
